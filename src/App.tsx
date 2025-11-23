27b885d4be365e10f3badc8ea3d1c76b8fc5aced
lee0jae330 <xingxing2001@cau.ac.kr>
2025-11-24 02:48:50 +0900

    feat: hashRouter로 교체

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomePage } from '@/pages/home';
 import { ProductDetailPage } from '@/pages/product-detail';
 import { BaseLayout } from '@/widgets/layout/BaseLayout';
